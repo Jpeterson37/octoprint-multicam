153763f622336a04d0c5ec4a1d7896c18656c4f4
Jpeterson37 <joshuapeterson37@yahoo.com>
2020-12-15 21:22:21 +0000

    inital

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-sudo mkdir /octoprint && sudo chmod 644 /octoprint
-sudo chmod +x ./octoprint-service && sudo cp ./octoprint-service /octoprint
-sudo cp ./dock* /octoprint/*
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+mkdir /octoprint && chmod 777 /octoprint
+chmod +x octoprint-service && sudo cp octoprint-service /octoprint/
+cp dock* /octoprint/
+cp octoprint.service /etc/systemd/system/
+systemctl enable octoprint
+systemctl start octoprint
